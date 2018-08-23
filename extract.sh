48fbaea5e30d19f36f874a516fe2c40951e602e3
lessthanjake <jacobbharris@gmail.com>
2018-08-23 13:56:44 -0600

    Update extract.sh
    Commented out the script

--- extract.sh
+++ extract.sh
@@ -1,7 +1,7 @@
-for i in *.tar.gz
+for i in *.tar.gz	#gets all the files in the current directory with .tar.gz
 do
-	NAME=${i%.tar.gz}
-	NAME2=${NAME##*$}
-	echo Extracting $i into folder $NAME2
-	tar -xvzf "$i" --owner processor --group processor --no-same-owner --xform="s|^|$NAME2/|S"          #run command
+	NAME=${i%.tar.gz}	#strips off the .tar.gz
+	NAME2=${NAME##*$}	#strips off the prefix up to the last $ in the file name
+	echo Extracting $i into folder $NAME2	#Show on screen what is going on
+	tar -xvzf "$i" --owner processor --group processor --no-same-owner --xform="s|^|$NAME2/|S"          #run tar extraction assigning certain rights and not worrying about original owner into the folder named $NAME2
 done
